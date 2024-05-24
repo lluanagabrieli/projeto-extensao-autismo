1abfaeda5a322900c9f863e9908983f2060c399b
lluanagabrieli <luanagabrieli4599@hotmail.com>
2024-05-24 17:40:45 -0300

    feat: adiciona mais um som para teste

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-document.getElementById("playButton").addEventListener("click", function () {
-  var audio = document.getElementById("myAudio");
-  audio.play();
+// document.getElementById("playButton").addEventListener("click", function () {
+//   var audio = document.getElementById("myAudio");
+//   audio.play();
+// });
+
+document.querySelectorAll(".playButton").forEach((button) => {
+  button.addEventListener("click", function () {
+    var audioPlayer = document.getElementById("audioPlayer");
+    var audioSrc = this.getAttribute("data-audio");
+    if (audioPlayer.src.includes(audioSrc)) {
+      if (audioPlayer.paused) {
+        audioPlayer.play();
+      } else {
+        audioPlayer.pause();
+      }
+    } else {
+      audioPlayer.src = audioSrc;
+      audioPlayer.play();
+    }
+  });
 });
